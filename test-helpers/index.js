8da4822a6f0f000603507701f5458190197f9eb3
rgargya <robert.gargya@emarsys.com>
2020-04-25 15:14:06 +0200

    skip vue warnings for unknown UI framework custom elements

--- test-helpers/index.js
+++ test-helpers/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 Vue.config.devtools = false;
 Vue.config.productionTip = false;
+Vue.config.ignoredElements = [/^e-/];
 
 // require all test-helpers files (files that ends with .spec.js)
 const testsContext = require.context('./', true, /\.spec$/);
