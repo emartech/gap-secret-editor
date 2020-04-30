7481e5ff259beafb1cdc980ca676896a8570b86a
rgargya <robert.gargya@emarsys.com>
2020-04-30 12:29:23 +0200

    SUITEDEV-22296: say goodbye to sayHello

--- src/renderer/components/app/app.spec.js
+++ src/renderer/components/app/app.spec.js
@@ -2,10 +2,4 @@ import { shallowMount } from '@vue/test-utils';
 import App from './app';
 
 describe('App', () => {
-  describe('#sayHello', () => {
-    it('should say hello', () => {
-      const { vm } = shallowMount(App);
-      expect(vm.sayHello('Tim')).to.eql('Hello Tim!');
-    });
-  });
 });
